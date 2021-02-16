2ddea37eefac1b71fab5036e8890e0d7987128b1
hoochanlon <hoochanlon@outlook.com>
2021-02-16 10:53:11 +0800

    asd

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -103,7 +103,8 @@ module.exports = {
                   ['/enhance/xiangm/xmgs','项目概述与项目经理'],
                   ['/enhance/xiangm/xmzy','项目组及整体管理'],
                   ['/enhance/xiangm/xmfw','项目范围实施过程'],
-                  ['/enhance/xiangm/xmjd','项目进度预估及管控']
+                  ['/enhance/xiangm/xmjd','项目进度预估及管控'],
+                  ['/enhance/xiangm/xmcb','项目成本管理']
                 ]
               },
               {
